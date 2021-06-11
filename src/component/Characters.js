fb4c1c026baca07ac9aacfac300c244c8efa9655
suleymankhrmn <skahraman1834@gmail.com>
2021-06-11 23:04:16 +0300

    area was edted

--- src/component/Characters.js
+++ src/component/Characters.js
@@ -9,9 +9,13 @@ const Characters = () => {
   const [total, setTotal] = useState(0);
 
   const fetchData = (page, size) => {
-    fetch("https://anapioficeandfire.com/api/characters?page=" +page +"&pageSize=" +size )
-      .then((res) => 
-        res.json())
+    fetch(
+      "https://anapioficeandfire.com/api/characters?page=" +
+        page +
+        "&pageSize=" +
+        size
+    )
+      .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setData(data);
@@ -22,9 +26,9 @@ const Characters = () => {
     fetchData(page, pageSize);
   }, []);
 
-  const showData = (data)=>{
+  const showData = (data) => {
     return data.length === 0 ? "Unknown" : data;
-  }
+  };
 
   const columns = [
     {
@@ -32,34 +36,31 @@ const Characters = () => {
       accessor: "name​",
       cell: (row) => <div>{row.name}</div>,
     },
+
     {
       name: "Alive",
       accessor: "createdDate",
       cell: (row) => <div>{row.died.length === 0 ? "Yes" : "No"}</div>,
     },
+
     {
-      name: <GenderFilter />,
+      name: "Gender",
       accessor: "gender",
       cell: (row) => <div>{showData(row.gender)}</div>,
     },
+
     {
       name: "Culter",
       accessor: "culture",
       cell: (row) => <div>{showData(row.culture)}</div>,
     },
 
-    // todo: romuald will add other column
-
-  {
-      name: Culture,
-      accessor: "culture",
-      cell: (row) => <div>{showData(row.culture)}</div>,
-    },
     {
       name: "Allegiances",
       accessor: "allegiances",
       cell: (row) => <div>{row.allegiances.length}</div>,
     },
+
     {
       name: "Books",
       accessor: "books",
@@ -70,14 +71,19 @@ const Characters = () => {
   ];
 
   return (
-    <SmartTable
-      columns={columns}
-      fetchData={fetchData}
-      data={data}
-      total={total}
-      page={page}
-      size={pageSize}
-    />
+    <div className="col">
+      <div className="row">filter component is here</div>
+      <div className="row">
+        <SmartTable
+          columns={columns}
+          fetchData={fetchData}
+          data={data}
+          total={total}
+          page={page}
+          size={pageSize}
+        />
+      </div>
+    </div>
   );
 };
 
